ff7648ca12e67c80c6f5e498baf7244ecda6fe65
Gabriele Bisceglie <bisceglie13200670@luigidellerba.edu.it>
2025-04-28 18:14:01 +0200

    ig_button fix

--- js/instagramButton.js
+++ js/instagramButton.js
@@ -1,22 +1,25 @@
 document.querySelector('.link-card i.fab.fa-instagram').parentElement.addEventListener('click', (e) => {
-    e.preventDefault();
-  
-    const instagramAppUrl = "instagram://user?username=gabriele.bisceglie_"; // Sostituisci "tuo_username" con il tuo username Instagram
-    const instagramWebUrl = "https://www.instagram.com/gabriele.bisceglie_"; // Sostituisci "tuo_username" con il tuo username Instagram
-  
-    // Controlla se l'utente è su un dispositivo mobile
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  
-    if (isMobile) {
+  e.preventDefault();
+
+  const instagramAppUrl = "instagram://user?username=gabriele.bisceglie_";
+  const instagramWebUrl = "https://www.instagram.com/gabriele.bisceglie_";
+  // Controlla se l'utente è su un dispositivo mobile
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
       // Prova ad aprire l'app di Instagram
+      const startTime = Date.now();
       window.location.href = instagramAppUrl;
-  
-      // Se l'app non è installata, apri il sito web dopo un breve ritardo
+
+      // Controlla se l'utente è tornato sulla pagina dopo un breve intervallo
       setTimeout(() => {
-        window.location.href = instagramWebUrl;
-      }, 500);
-    } else {
+          const elapsedTime = Date.now() - startTime;
+          if (elapsedTime < 1500) { // Se l'utente è tornato rapidamente, apri il sito web
+              window.location.href = instagramWebUrl;
+          }
+      }, 1000);
+  } else {
       // Se non è un dispositivo mobile, apri il sito web
       window.open(instagramWebUrl, "_blank");
-    }
-  });
+  }
+});
